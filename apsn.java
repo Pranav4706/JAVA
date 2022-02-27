a7eef5d7e1e23f0f58d23e360e53e863733a6fc2
Pranav4706 <70790500+Pranav4706@users.noreply.github.com>
2022-02-27 08:56:37 +0530

    Create apsn.java

---
+++ apsn.java
@@ -0,0 +1,21 @@
+public class apsn {
+    public static void main(String[] args) {
+        String s = "Aditya Pratap Singh Negi";
+        s = s.trim();
+        s = " " + s;
+        int p = s.lastIndexOf(' ');
+        String lastname = s.substring(p+1);
+        s = s.substring(0, p);
+        s = s.toUpperCase();
+        String n = "";
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if(s.charAt(i) == ' ')
+            {
+               n = n + s.charAt(i+1) + ".";
+            }
+        }
+        n = n + " " + lastname;
+        System.out.println(n);
+    }
+}
